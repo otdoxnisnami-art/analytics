046527ecfdbfbc59830db91ee4cd29defd0f400f
Coronelly <otdoxnisnami@gmail.com>
2026-01-06 01:12:07 +0300

    Add files via upload

---
+++ data_maining.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def get_dataframe_shape(file_path):
+    df = pd.read_csv(file_path)
+    return df.shape
+
+def get_dataframe_dtypes(file_path):
+    df =  pd.read_csv(file_path)
+    return df.dtypes
