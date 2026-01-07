c41b010e3b9e938235a7b5a4b9874b9f7062f07c
Сулейман Нахмедов <sulejmannahmedov@Mac.IGD_Rostelecom>
2026-01-08 00:08:30 +0300

    add utf-8 encoding

--- data_maining.py
+++ data_maining.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def get_dataframe_shape(file_path):
-    df = pd.read_csv(file_path, encoding='windows-1251')
+    df = pd.read_csv(file_path, encoding='utf-8')
     return df.shape
 
 def get_dataframe_dtypes(file_path):
